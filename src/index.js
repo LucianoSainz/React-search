c4b61d3280e6e2707ab500db57a056e033ff67ad
luciaoSainz <danielsainz28@gmail.com>
2020-05-04 10:00:18 +0200

    added cosntrutor & super in index

--- src/index.js
+++ src/index.js
@@ -1,19 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+const API_KEY = ''
+
 
 YTSearch({ key: API_KEY, term: 'surfboards' }, function (data) {
     console.log(data);
 });
 
-const API_KEY = ''
 
-const App = () => {
-    return <div>
-        <SearchBar />
-    </div>;
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { videos: [] };
+    }
+
+    render() {
+        return (
+            <div>
+                <SearchBar />
+            </div>
+        );
+    }
 }
-
 
 ReactDOM.render(<App />, document.querySelector('.container'));
