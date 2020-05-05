4755df5810bf64d085ec2b603cbf834a8d3591b2
luciaoSainz <danielsainz28@gmail.com>
2020-05-05 09:20:06 +0200

    added ul in videoList

--- src/index.js
+++ src/index.js
@@ -5,17 +5,16 @@ import SearchBar from './components/search_bar';
 const API_KEY = ''
 
 
-YTSearch({ key: API_KEY, term: 'surfboards' }, function (data) {
-    console.log(data);
-});
-
-
-
 class App extends Component {
     constructor(props) {
         super(props);
 
         this.state = { videos: [] };
+
+
+        YTSearch({ key: API_KEY, term: 'surfboards' }, (videos) => {
+            this.setState({ videos });
+        });
     }
 
     render() {
