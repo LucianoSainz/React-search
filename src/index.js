188c4129d2950d93dd7379084a3c7c7ff483647f
luciaoSainz <danielsainz28@gmail.com>
2020-05-08 10:05:38 +0200

    refactoring in index

--- src/index.js
+++ src/index.js
@@ -11,9 +11,9 @@ class App extends Component {
     constructor(props) {
         super(props);
 
-        this.state = { 
-            videos: [], 
-            selectedVideo: null;
+        this.state = {
+            videos: [],
+            selectedVideo: null
         };
 
 
@@ -26,7 +26,7 @@ class App extends Component {
         return (
             <div>
                 <SearchBar />
-                <VideoDetail video={this.state.videos[0]}/>
+                <VideoDetail video={this.state.selectedVideo} />
                 <VideoList videos={this.state.videos} />
             </div>
         );
