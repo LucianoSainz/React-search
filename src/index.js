34fe71c95e8e6bbeb656ae12af79edf1ac814f61
luciaoSainz <danielsainz28@gmail.com>
2020-05-11 10:05:02 +0200

    refactoring

--- src/index.js
+++ src/index.js
@@ -31,9 +31,10 @@ class App extends Component {
 
     render() {
         const videoSearch = _.debounce((term) => { this.videoSearch(term) }, 300);
+
         return (
             <div>
-                <SearchBar onSearchTermChange={term => this.videoSearch(TextDecoderStream)} />
+                <SearchBar onSearchTermChange={videoSearch} />
                 <VideoDetail video={this.state.selectedVideo} />
                 <VideoList
                     onVideoSelect={selectedVideo => this.setState({ selectedVideo })}
